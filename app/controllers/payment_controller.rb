8b2999cd465b979c1264ba7a4673653a375fb9c0
Rayan Castro <rayancdc@gmail.com>
2020-06-10 20:22:39 -0300

    Update checkout plan to price

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -2,15 +2,13 @@ class PaymentController < ApplicationController
   def checkout
     subscription_plan = ENV['MONTHLY_STRIPE_PLAN']
 
+    # Atualizado para utilização da PRICES API
     @session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
-      subscription_data: {
-        items: [
-          {
-            plan: subscription_plan
-          }
-        ]
-      },
+      line_items: [{
+        price: subscription_plan,
+        quantity: 1
+      }],
       success_url: checkout_success_url,
       cancel_url: checkout_error_url,
     )
